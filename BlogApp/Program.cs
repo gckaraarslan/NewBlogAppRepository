f1690e7831bc8d693531709c89596d54cdde9831
Galip Can KARAARSLAN <gckaraarslan@gmail.com>
2024-02-01 23:44:48 +0300

    Comment Save and Ajax Request

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<BlogContext>(options=>{
 });
 builder.Services.AddScoped<IPostRepository,EfPostRepository>();
 builder.Services.AddScoped<ITagRepository,EfTagRepository>();
+builder.Services.AddScoped<ICommentRepository,EfCommentRepository>();
 var app = builder.Build();
 app.UseStaticFiles();
 
--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1bb7b848ff06975403a7920c87b5bdf653d276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6002fc98f48f563d0f9deadd308211060c9d589b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
