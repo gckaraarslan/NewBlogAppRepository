bf1e814179f3275f09491fe06438d358d262a30d
Galip Can KARAARSLAN <gckaraarslan@gmail.com>
2024-02-26 22:49:57 +0300

    User Register Form

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c096eeb6404df3fc5e761554e5c50b4fb21df04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee3dcff13921b8563fb7d2c0e7ab6bd2d4582d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
